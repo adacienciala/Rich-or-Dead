bcfa6c17b215ef017c2af977ca2fc145dd9ed92d
root <adacien@o2.pl>
2019-12-22 18:58:17 +0100

    Add print_board and load_board

---
+++ server.c
@@ -0,0 +1,248 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ncurses.h>
+#include <semaphore.h>
+
+#define MS 1000
+
+// colors stuff
+#define C_PLAYER 1
+#define C_WALL 2
+#define C_BEAST 3
+#define C_MONEY 4
+#define C_CAMPSITE 5
+#define C_DROPPED 6
+#define C_DEFAULT 7
+
+
+// board stuff
+#define ROWS 25
+#define COLUMNS 51
+
+char board[ROWS][COLUMNS];
+int max_x, max_y;
+
+// game stuff
+#define MAX_PLAYERS 4
+
+// semaphores
+// 4 semafory z danymi do druku
+
+
+
+
+int load_board(char *filename);
+void* print_board(void* none);
+void* key_events(void *none);
+
+int main(void)
+{
+    srand(time(NULL));
+    initscr();
+    noecho();
+    curs_set(FALSE);
+    keypad(stdscr, TRUE);
+    getmaxyx(stdscr, max_y, max_x);
+
+    load_board("board.txt");
+
+    pthread_t printing_thread;
+    pthread_create(&printing_thread, NULL, print_board, NULL);
+
+    return 0;
+}
+
+int load_board(char *filename)
+{
+    if (!filename) return 1;
+
+    FILE *f;
+    if ((f=fopen(filename, "r")) == NULL ) return 2;
+
+    int row = 0;
+
+    while(!feof(f) && row < ROWS)
+    {
+        fscanf(f, "%s", board[row++]);
+    }
+
+    fclose(f);
+    return 0;
+}
+
+void* print_board(void* none)
+{
+    start_color();
+
+    init_color(COLOR_BLUE, 100, 400, 700);
+    init_color(COLOR_YELLOW, 1000, 1000, 0);
+    init_color(COLOR_WHITE, 1000, 1000, 1000);
+
+    init_pair(C_PLAYER, COLOR_WHITE, COLOR_BLUE);
+    init_pair(C_WALL, COLOR_BLACK, COLOR_BLACK);
+    init_pair(C_BEAST, COLOR_RED, COLOR_WHITE);
+    init_pair(C_MONEY, COLOR_BLACK, COLOR_YELLOW);
+    init_pair(C_CAMPSITE, COLOR_YELLOW, COLOR_GREEN);
+    init_pair(C_DROPPED, COLOR_GREEN, COLOR_YELLOW);
+    init_pair(C_DEFAULT, COLOR_BLACK, COLOR_WHITE);
+
+    // terminal background
+    attron(COLOR_PAIR(C_DEFAULT));
+    for (int i = 0; i < max_y; ++i)
+    {
+        for (int j = 0; j < max_x; ++j)
+        {
+            mvprintw(i, j, " ");
+        }
+    }
+
+    while(1)
+    {
+        // board
+        for (int i = 0; i < ROWS; ++i)
+        {
+            for (int j = 0; j < COLUMNS; ++j)
+            {
+                // players and beasts
+
+                // the board and artefacts
+                switch (board[i][j])
+                {
+                    case '|':
+                        attron(COLOR_PAIR(C_WALL));
+                        break;
+                    case 'c':
+                    case 't':
+                    case 'T':
+                        attron(COLOR_PAIR(C_MONEY));
+                        break;
+                    case 'A':
+                        attron(COLOR_PAIR(C_CAMPSITE));
+                        break;
+                    case 'D':
+                        attron(COLOR_PAIR(C_DROPPED));
+                        break;
+                    case '.':
+                        board[i][j] = ' ';
+                    default:
+                        attron(COLOR_PAIR(C_DEFAULT));
+                }
+                mvprintw(i, j, "%c", board[i][j]);
+            }
+        }
+
+        // server info
+        int cur_row = 0, cur_col = COLUMNS + 4;
+        attron(COLOR_PAIR(C_DEFAULT));
+        mvprintw(++cur_row, cur_col, "Server's PID: %d", -1);
+        mvprintw(++cur_row, cur_col + 1, "Campsite X/Y: %d/%d", -1, -1);
+        mvprintw(++cur_row, cur_col + 1, "Round number: %d", -1);
+
+        // players info
+        ++cur_row;
+        for (int id = 0; id < MAX_PLAYERS; ++id)
+        {
+            int player_connected = 0;
+            // if polaczony zmieniaj zmienna
+            mvprintw(++cur_row, cur_col, "Parameter:");
+            mvprintw(cur_row, cur_col + 13 + (id * 10), "Player%d", id + 1);
+            mvprintw(++cur_row, cur_col + 1, "PID");
+            if (player_connected) mvprintw(cur_row, cur_col + 13 + (id * 10), "%d", -1);
+            else mvprintw(cur_row, cur_col + 13 + (id * 10), "%c", '-');
+            mvprintw(++cur_row, cur_col + 1, "Type");
+            if (player_connected) mvprintw(cur_row, cur_col + 13 + (id * 10), "%s", "NONE");
+            else mvprintw(cur_row, cur_col + 13 + (id * 10), "%c", '-');
+            mvprintw(++cur_row, cur_col + 1, "Curr X/Y");
+            if (player_connected) mvprintw(cur_row, cur_col + 13 + (id * 10), "%d/%d", -1, -1);
+            else mvprintw(cur_row, cur_col + 13 + (id * 10), "%s", "--/--");
+            mvprintw(++cur_row, cur_col + 1, "Deaths");
+            if (player_connected) mvprintw(cur_row, cur_col + 13 + (id * 10), "%d", -1);
+            else mvprintw(cur_row, cur_col + 13 + (id * 10), "%c", '-');
+            mvprintw(++cur_row, cur_col + 1, "Coins");
+            mvprintw(++cur_row, cur_col + 2, "carried");
+            if (player_connected) mvprintw(cur_row, cur_col + 13 + (id * 10), "%d", -1);
+            else mvprintw(cur_row, cur_col + 13 + (id * 10), "%c", '-');
+            mvprintw(++cur_row, cur_col + 2, "brought");
+            if (player_connected) mvprintw(cur_row, cur_col + 13 + (id * 10), "%d", -1);
+            else mvprintw(cur_row, cur_col + 13 + (id * 10), "%c", '-');
+            cur_row -= 8;
+        }
+
+        // legend
+        cur_row += 9;
+        mvprintw(++cur_row, cur_col, "Legend:");
+        attron(COLOR_PAIR(C_PLAYER));
+        mvprintw(++cur_row, cur_col + 1, "1234");
+        attron(COLOR_PAIR(C_WALL));
+        mvprintw(++cur_row, cur_col + 1, " ");
+        attron(COLOR_PAIR(C_DEFAULT));
+        mvprintw(++cur_row, cur_col + 1, "#");
+        attron(COLOR_PAIR(C_BEAST));
+        mvprintw(++cur_row, cur_col + 1, "*");
+        attron(COLOR_PAIR(C_MONEY));
+        mvprintw(++cur_row, cur_col + 1, "c");
+        mvprintw(++cur_row, cur_col + 1, "t");
+        mvprintw(++cur_row, cur_col + 1, "T");
+        attron(COLOR_PAIR(C_CAMPSITE));
+        mvprintw(++cur_row, cur_col + 1, "A");
+        attron(COLOR_PAIR(C_DROPPED));
+        mvprintw(++cur_row, cur_col + 1, "D");
+        cur_row -= 9;
+        attron(COLOR_PAIR(C_DEFAULT));
+        mvprintw(++cur_row, cur_col + 7, "- players");
+        mvprintw(++cur_row, cur_col + 7, "- wall");
+        mvprintw(++cur_row, cur_col + 7, "- bush (slow down)");
+        mvprintw(++cur_row, cur_col + 7, "- wild beast");
+        mvprintw(++cur_row, cur_col + 7, "- one coin");
+        mvprintw(++cur_row, cur_col + 7, "- treasure (10 coins)");
+        mvprintw(++cur_row, cur_col + 7, "- large treasure (50 coins)");
+        mvprintw(++cur_row, cur_col + 7, "- campsite");
+        mvprintw(++cur_row, cur_col + 7, "- dropped treasure");
+
+        refresh();
+        usleep(500 * MS);
+    }
+}
+
+void* key_events(void *none)
+{
+    // constantly listening to the keyboard
+    char a;
+    while(1)
+    {
+        a = getchar();
+        switch(a)
+        {
+            // quit the game
+            case 'q':
+            case 'Q':
+                //postnij semafor zamykajacy gre
+                return NULL;
+            // add a new beast
+            case 'b':
+            case 'B':
+                //watek dodania bestii
+                break;
+            // add a coin/treasure/large treasure
+            case 'c':
+            case 't':
+            case 'T':
+                while (1)
+                {
+                    int x = rand() % COLUMNS;
+                    int y = rand() % ROWS;
+                    if (board[y][x] == ' ')
+                    {
+                        board[y][x] = a;
+                        break;
+                    }
+                }
+                break;
+        }
+    }
+}
